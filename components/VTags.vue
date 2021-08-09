fb5f0c63377859c2945751a9d3e2db920846cde0
brianssparetime <brianssparetime@gmail.com>
2021-08-08 21:39:32 -0700

    css fix for tag background coverage on right

--- components/VTags.vue
+++ components/VTags.vue
@@ -19,11 +19,9 @@ export default {
 
 <style scoped>
 .tag a {
-  background: #41b38a;
   color: white;
   text-decoration: none;
   margin: 5px 0;
-  padding: 1px 3px;
   display: inline-block;
   border-radius: 3px;
 }
